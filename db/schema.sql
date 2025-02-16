184a90d73111bc877bc76f341dfa63e2d1e0ec93
Aleksander Oleszkiewicz <aleksander.oleszkiewicz0@gmail.com>
2025-02-16 12:00:54 +0100

    Delete db directory

--- db/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS game (
-    game_id INTEGER IDENTITY PRIMARY KEY,
-    player_x VARCHAR(50),
-    player_o VARCHAR(50),
-    winner VARCHAR(1),
-    date_time TIMESTAMP
-); 
